0c2b3756c5864d0481952673fad5b17de9538c5c
Fredy Sarmiento <fasarmiento93@gmail.com>
2020-05-25 17:48:36 -0500

    Elemento creado y unificadoa API para integracion

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,40 +1,47 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
+import useInitialState from '../Hooks/useInitialState'
 import '../assets/styles/App.scss';
 
-const App=()=>(
-    <div className>
-    <Header/>
-    <Search/>
-    <Categories title="Mi Lista">
+const API='http://localhost:3000/initalState/';
+
+const App = () => {
+    const initialState = useInitialState(API);
+    return initialState.length === 0 ? <h1>Loading...</h1> : (
+        <div className="App">
+            <Header />
+            <Search />
+        {initialState.mylist.length > 0 &&
+        <Categories title="Mi Lista">
+            <Carousel>
+            {initialState.mylist.map(item =>
+                <CarouselItem key={item.id} {...item} />
+            )}
+            </Carousel>
+        </Categories>
+        }
+        <Categories title="Tendencias">
         <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
+            {initialState.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />
+            )}
         </Carousel>
-    </Categories>
-    <Categories title="Tendencias">
+        </Categories>
+        <Categories title="Originales de Platzi Video">
         <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-            <CarouselItem/>
+            {initialState.originals.map(item =>
+            <CarouselItem key={item.id} {...item} />
+            )}
         </Carousel>
-    </Categories>
-    <Categories title="Originales">
-        <Carousel>
-            <CarouselItem/>
-            <CarouselItem/>
-        </Carousel>
-    </Categories>
-    <Footer/>
+        </Categories>
+        <Footer />
     </div>
-);
+    );
+}
 
 export default App;
