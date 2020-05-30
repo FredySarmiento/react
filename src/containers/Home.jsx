45b2404be09bc0fb8ce1958b06e08183a18887e0
Fredy Sarmiento <fasarmiento93@gmail.com>
2020-05-30 15:46:14 -0500

    Creacion de layout not found y instalacion de redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,22 +1,20 @@
 import React,{useState,useEffect} from 'react';
-import Header from '../components/Header';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
-import useInitialState from '../Hooks/useInitialState'
+import useInitialState from '../Hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API='http://localhost:3000/initalState/';
+/*cuando se necesita hooks const API='http://localhost:3000/initalState/';*/
 
-const Home = () => {
-    const initialState = useInitialState(API);
-    return initialState.length === 0 ? <h1>Loading...</h1> : (
-        <div className="App">
-            <Header />
+const Home = ({myList,trends,originals}) => {
+    /*Uso de hooks const initialState = useInitialState(API);*/
+    return /* validacion de si tengo o no datos en la api initialState.length === 0 ? <h1>Loading...</h1> : */(
+        <>
             <Search />
-        {initialState.mylist.length > 0 &&
+        {myList.length > 0 &&
         <Categories title="Mi Lista">
             <Carousel>
             {initialState.mylist.map(item =>
@@ -27,21 +25,28 @@ const Home = () => {
         }
         <Categories title="Tendencias">
         <Carousel>
-            {initialState.trends.map(item =>
+            {trends.map(item =>
             <CarouselItem key={item.id} {...item} />
             )}
         </Carousel>
         </Categories>
         <Categories title="Originales de Platzi Video">
         <Carousel>
-            {initialState.originals.map(item =>
+            {originals.map(item =>
             <CarouselItem key={item.id} {...item} />
             )}
         </Carousel>
         </Categories>
-        <Footer />
-    </div>
+    </>
     );
 }
 
-export default Home;
+const mapStateToProps=state=>{
+    return{
+        myList:state.myList,
+        trends:state.trends,
+        originals:state.originals,
+    }
+}
+//export default connect(props, actions)(Home);
+export default connect(mapStateToProps, null)(Home);
---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () =>(
+    <>
+    <h1>No Encontrado </h1>
+    <h2>Vuelve al Home</h2>
+    </>
+);
+
+export default NotFound;
